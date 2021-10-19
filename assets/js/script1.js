e4642daa68324a566e69c0ab6da6c54e7e6fa411
Arnaud Losson <arnaudlosson3@gmail.com>
2021-10-19 11:17:16 +0200

    modif branche test

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -2,10 +2,18 @@ let gameInfo = {
     graine: 0,
     tournesol: 0,
     pirate: 0,
-    divin: 0 
+    divin: 0,
+    bagPrice: 0,
+    tournesolPrice: 0,
+    piratePrice: 0,
+    divinPrice: 0
 }
 
-let viewGraine = document.getElementById("viewGraine").innerHTML;
+let bagPrice = 0;
+let tournesolPrice = 0;
+let piratePrice = 0;
+let divinPrice = 0;
+let viewGraine = document.getElementById("viewGraine");
 let title = document.getElementById("title").innerHTML;
 let multiplier1 = document.getElementById("x1");
 let multiplier10 = document.getElementById("x10");
@@ -14,6 +22,18 @@ let multiplierX1 = document.getElementById("x1");
 let multiplierX10 = document.getElementById("x10");
 let multiplierX100 = document.getElementById("x100");
 let multiplierBuy;
+let clickMultiplier = 1;
+let clickSeeds = document.getElementById("clickSeeds");
+
+let viewPriceBag = document.getElementById("prizeBag");
+let viewPriceSun = document.getElementById("prizeSun");
+let viewPriceParrot = document.getElementById("prizeParrot");
+let viewPriceStatue = document.getElementById("prizeStatue");
+
+let gameLoad = (() => {
+    //initialisation
+
+}) 
 
 multiplierX1.addEventListener("click", () => {
     multiplierBuy = "1";
@@ -37,3 +57,59 @@ multiplierX100.addEventListener("click", () => {
     console.log(multiplierBuy);
 });
 
+clickSeeds.addEventListener("click", () => {
+    viewGraine.innerHTML = parseInt(viewGraine.innerHTML) + clickMultiplier;
+    console.log("+1 graine");
+})
+//affichage du prix du bag en hover
+document.getElementById("seedBag").addEventListener("mouseover", () => {
+    document.getElementsByClassName("prizeBag")[0].style.visibility = "visible";
+})
+document.getElementById("seedBag").addEventListener("mouseleave", () => {
+    document.getElementsByClassName("prizeBag")[0].style.visibility = "hidden";
+})
+
+//affichage du prix du tournesol en hover
+document.getElementById("sunflower").addEventListener("mouseover", () => {
+    document.getElementsByClassName("prizeSun")[0].style.visibility = "visible";
+})
+document.getElementById("sunflower").addEventListener("mouseleave", () => {
+    document.getElementsByClassName("prizeSun")[0].style.visibility = "hidden";
+})
+
+//affichage du prix du pirate en hover
+document.getElementById("pirate").addEventListener("mouseover", () => {
+    document.getElementsByClassName("prizeParrot")[0].style.visibility = "visible";
+})
+document.getElementById("pirate").addEventListener("mouseleave", () => {
+    document.getElementsByClassName("prizeParrot")[0].style.visibility = "hidden";
+})
+
+//affichage du prix de la statue en hover
+document.getElementById("statue").addEventListener("mouseover", () => {
+    document.getElementsByClassName("prizeStatue")[0].style.visibility = "visible";
+})
+document.getElementById("statue").addEventListener("mouseleave", () => {
+    document.getElementsByClassName("prizeStatue")[0].style.visibility = "hidden";
+})
+
+document.getElementById("seedBag").addEventListener("click", () => {
+    if (buy(gameInfo.graine, gameInfo.bagPrice)) {
+        
+    }
+})
+document.getElementById("sunflower").addEventListener("click", () => {
+    if (buy(gameInfo.graine, gameInfo.tournesolPrice)) {
+        
+    }
+})
+document.getElementById("pirate").addEventListener("click", () => {
+    if (buy(gameInfo.graine, gameInfo.piratePrice)) {
+        
+    }
+})
+document.getElementById("statue").addEventListener("click", () => {
+    if (buy(gameInfo.graine, gameInfo.divinPrice)) {
+        
+    }
+})
